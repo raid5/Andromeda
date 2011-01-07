95fce37d363b5dbc5682c68de85c0c75ade9c278
Adam McDonald <mcdonald.adam@gmail.com>
2011-01-06 16:52:45 -0800

    Initial GitHub commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Planet::Application.config.secret_token = '178d26e34072b21da37d692bbbb3b7c3f307c6247af8981efa4d659c589955de74afac2e95c9d47d3ca1e0fc3225648b522f4e6e9dbd7cc3ad7876f8079ad5fb'
